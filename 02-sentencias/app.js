8d978bfed46fd9849f55a93ba38547e3671ec0a2
Andres Amuchastegui <amuchastegui1994@gmail.com>
2022-08-30 21:04:15 -0300

    Setencias Switch

--- 02-sentencias/app.js
+++ 02-sentencias/app.js
@@ -1,12 +1,50 @@
-var nombre = "Andrés"
-var edad = 60
+// var nombre = "Andrés"
+// var edad = 60
 
-if (edad < 12) {
-    console.log(nombre, ' es un niño')
-} else if ((edad > 11) && (edad < 18)) {
-    console.log(nombre, ' es un adolecente')
-} else if ((edad > 17) && (edad < 60)) {
-    console.log(nombre, ' en un adulto')
-} else {
-    console.log(nombre, ' es un asiano')
-}
+// if (edad < 12) {
+//     console.log(nombre, ' es un niño')
+// } else if ((edad > 11) && (edad < 18)) {
+//     console.log(nombre, ' es un adolecente')
+// } else if ((edad > 17) && (edad < 60)) {
+//     console.log(nombre, ' en un adulto')
+// } else {
+//     console.log(nombre, ' es un asiano')
+// }
+
+/****************************** */
+/*Operador ternario*/
+
+// var nombre = 'Andrés'
+// var edad = 17
+
+// edad >= 18 ? console.log(nombre, ' es mayor de edad') :
+//     console.log(nombre, ' es menor de edad')
+
+
+/***************************************** 
+Sentencia Switch 
+******************************************/
+
+// var meses = 4;
+// switch (meses) {
+//     case 1:
+//         console.log('Enero');
+//         break;
+//     case 2:
+//         console.log('Febrero');
+//         break;
+//     case 3:
+//         console.log('Marzo');
+//         break
+//     case 4:
+//         console.log('Abril');
+//         break
+//     default:
+//         console.log('Mes no considerado')
+// }
+
+/***************************************** 
+Sentencias For
+
+******************************************/
+
