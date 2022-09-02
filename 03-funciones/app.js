e6fa359fe10ce25c0c0d60be36c41fd82f3d3272
Andrés Amuchástegui <amuchastegui1994@gmail.com>
2022-09-02 11:41:31 -0300

    funcion convertir F a C°

--- 03-funciones/app.js
+++ 03-funciones/app.js
@@ -20,3 +20,18 @@ var num = 5
 var valor = cuadradoNumero(num)
 console.log(valor)
 
+
+// FUncion que convierte de grados Fahrenheint a Celcius
+
+// 32F = 0C, 68F = 20C.
+// c = (F - 32) * 5/9
+
+function convertirFAHaCelsius(gradoFah) {
+    var celsius = (gradoFah - 32) * 5 / 9
+    return celsius
+}
+
+var tempUno = convertirFAHaCelsius(32)
+var tempDos = convertirFAHaCelsius(68)
+console.log(tempUno)
+console.log(tempDos)
