48af276837ea04651740e4503f96f8725220b38d
Andrés Amuchástegui <amuchastegui1994@gmail.com>
2022-09-02 11:17:59 -0300

    Ejercicios

--- 02-sentencias/app.js
+++ 02-sentencias/app.js
@@ -50,7 +50,7 @@ Sentencias For
 
 // for (var i = 0; i <= 10; i++) {
 //     console.log(i)
-    
+
 // }
 
 
@@ -76,18 +76,71 @@ Sentencias For
 //     console.log(x)
 // } while (x <= 10)
 
-var edad = '10' // JS lo declara como valor falso
+// var edad = '10' // JS lo declara como valor falso
 
-if (edad){
-    console.log('Variable esta definida')
-}else{
-    console.log('Variable no definida')
+// if (edad){
+//     console.log('Variable esta definida')
+// }else{
+//     console.log('Variable no definida')
+// }
+
+// // Operadores de igualdad
+
+// if(edad === 10){
+//     console.log('Variable con coersión')
+// }else {
+//     console.log('Variable sin coersión')
+// }
+
+// Ejercicio
+/*
+* Tienes dos alumnos, Pablo y Maria
+*Pablo tiene las siguientes notas en el curso de JS: 14, 8 16.
+*Maria tiene las siguientes notas en el mismo curso: 12, 18, 10.
+*
+* Calcular el promedio de cada alumno, ademas inidicar quien tiene el promedio
+*superior, e indicar si el alumno esta aprobado, para el su promedio
+*deber ser superior a 13 
+*/
+
+var Pablo = [14, 8, 16]
+let sumaPablo = 0;
+var Maria = [12, 18, 10]
+let sumaMaria = 0;
+
+
+for (let i = 0; i < Pablo.length; i++) {
+    sumaPablo += Pablo[i];
+}
+console.log(sumaPablo)
+
+for (let i = 0; i < Maria.length; i++) {
+    sumaMaria += Maria[i];
+}
+console.log(sumaMaria)
+
+let promedioPablo = sumaPablo / 3
+console.log('El promedio de Pablo es: ', promedioPablo)
+
+let promedioMaria = sumaMaria / 3
+console.log('El promedio de Maria es: ', promedioMaria)
+
+if (promedioPablo > 13) {
+    console.log('Pablo esta aprobado')
+} else {
+    console.log('Pablo a desaprobado')
 }
 
-// Operadores de igualdad
+if (promedioMaria > 13) {
+    console.log('Maria esta aprobada')
+} else {
+    console.log('Maria a desaprobado')
+}
 
-if(edad === 10){
-    console.log('Variable con coersión')
-}else {
-    console.log('Variable sin coersión')
+if (promedioPablo > promedioMaria) {
+    console.log('Pablo tiene el mejor promedio')
+} else if(promedioMaria > promedioPablo){
+    console.log('Maria tiene el mejor promedio')
+} else {
+    console.log('Pablo y Maria tienen el mismo promedio')
 }
